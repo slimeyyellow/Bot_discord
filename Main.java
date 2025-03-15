5116295da95c2e873b6ff177490d9a8151da1924
slimeyyellow <78305417+slimeyyellow@users.noreply.github.com>
2025-03-15 07:55:16 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,16 +8,19 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import net.dv8tion.jda.internal.utils.JDALogger;
 
 import java.util.EnumSet;
 
 public class Main {
     public static void main(String args[]) throws Exception{
         JDABuilder builder = JDABuilder.createDefault(
-                "Token");
+                "BOT TOKEN")
+                .enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS);
         builder.enableIntents(GatewayIntent.MESSAGE_CONTENT).setActivity(Activity.watching("You"));
         JDA jda = builder.build().awaitReady();
         jda.updateCommands().queue();
+        JDALogger.setFallbackLoggerEnabled(false);
 
         CommandListUpdateAction commands = jda.updateCommands();
         jda.awaitReady();
